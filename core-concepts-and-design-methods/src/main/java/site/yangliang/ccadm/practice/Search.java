ca961a8c3b7559eb99fdb5c0bbedfb6c8af5e535
YangHL <1183330249@qq.com>
2019-05-07 23:17:09 +0800

    search

--- core-concepts-and-design-methods/src/main/java/site/yangliang/ccadm/practice/Search.java
+++ core-concepts-and-design-methods/src/main/java/site/yangliang/ccadm/practice/Search.java
@@ -3,10 +3,12 @@ package site.yangliang.ccadm.practice;
 public class Search {
 
 	public static int search(int[] numbers, int key){
-		for(int i=0;i<numbers.length;i++)
-		if(numbers[i]==key)
-		return i;
-		return key;
+		for(int i=0;i<numbers.length;i++) {
+			if (numbers[i] == key) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 }
