1e75ca923c641d65c5c83577a648a5832ae2fe7e
YangHL <1183330249@qq.com>
2019-05-08 21:54:00 +0800

    修改Search类

--- core-concepts-and-design-methods/src/main/java/site/yangliang/ccadm/practice/Search.java
+++ core-concepts-and-design-methods/src/main/java/site/yangliang/ccadm/practice/Search.java
@@ -1,17 +1,33 @@
 package site.yangliang.ccadm.practice;
 
+
 public class Search {
 
 	/**
 	 * 查找
 	 * @param numbers 数组
 	 * @param key    要查找的元素
-	 * @return  查找的元素在数组中的位置
-	 */
-	public static int search(int[] numbers, int key){
-
-      return 0;
+	 * @return  查找的元素在数组中的位置*/
+	public static int search(int[] numbers, int key) throws MyException {
+		for(int i=0;i<numbers.length;i++) {
+			if (numbers[i] == key) {
+			return i;
+			}
+		}
+		throw new MyException(key);
 	}
+
+	}
+	class MyException extends Exception{
+		MyException(int key){
+		System.out.println("no this key"+key+"found");
+	}
+
+
+
+
+
+
 
 	/**
 	 * 二分查找
@@ -24,4 +40,14 @@ public class Search {
 		return 0;
 	}
 
+
+		public static void main(String[] args) {
+
+			try {
+				Search.search(new int[]{1,2},3);
+			}catch (MyException e){
+				e.printStackTrace();
+			}
+
+		}
 }
