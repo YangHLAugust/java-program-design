5bd8882d94578d446ccfb3190c2fe809b0aa749f
YangHL <1183330249@qq.com>
2019-05-08 17:25:43 +0800

    Forget what you taught me yesterday

--- core-concepts-and-design-methods/src/main/java/site/yangliang/ccadm/practice/Search.java
+++ core-concepts-and-design-methods/src/main/java/site/yangliang/ccadm/practice/Search.java
@@ -1,14 +1,25 @@
 package site.yangliang.ccadm.practice;
 
-public class Search {
+public class Search  {
 
-	public static int search(int[] numbers, int key){
+
+    public static int search(int[] numbers, int key) throws myexception {
 		for(int i=0;i<numbers.length;i++) {
 			if (numbers[i] == key) {
 				return i;
 			}
 		}
-		return -1;
+    try {
+        throw new myexception(key);
+    }catch (myexception e){
+        throw e;
+    }
 	}
 
 }
+class myexception extends Exception{
+    myexception(int key){
+        System.out.println("no this key"+key+"found");
+    }
+}
+
